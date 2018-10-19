2019f8328c92daac98fb2c67ce6e1a9179175be8
kkxz79 <kkxz79@126.com>
2018-10-19 14:31:34 +0800

    Initial Commit

---
+++ XZSingleton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZSingleton
+//
+//  Created by kkxz on 2018/10/19.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZSingleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZSingleton
+//
+//  Created by kkxz on 2018/10/19.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
