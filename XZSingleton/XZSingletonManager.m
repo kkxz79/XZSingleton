c7a5cf698ac385d05fa34682e13208b1e2ee3f0d
kkxz79 <kkxz79@126.com>
2018-10-19 15:57:50 +0800

    first commit

---
+++ XZSingleton/XZSingletonManager.m
@@ -0,0 +1,13 @@
+//
+//  XZSingletonManager.m
+//  XZSingleton
+//
+//  Created by kkxz on 2018/10/19.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import "XZSingletonManager.h"
+
+@implementation XZSingletonManager
+
+@end
